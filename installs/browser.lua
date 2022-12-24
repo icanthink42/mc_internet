be62caca2a33837b0934de53a4ac95e020bbd762
jackn <neelemanjack@gmail.com>
2022-12-24 11:50:52 -0500

    Removed the old version of the browser

--- installs/browser.lua
+++ installs/browser.lua
@@ -1,2 +1,5 @@
+shell.run("rm utils")
+shell.run("rm packet")
+shell.run("rm browser")
 shell.run("wget", "https://raw.githubusercontent.com/icanthink42/mc_internet/main/installs/browser_run.lua",
   "browser.lua")
