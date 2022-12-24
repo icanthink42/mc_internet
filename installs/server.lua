f958981eb37c8b8cf56963f2d5ed1690905c56a2
jackn <neelemanjack@gmail.com>
2022-12-24 11:27:25 -0500

    Added server install script

---
+++ installs/server.lua
@@ -0,0 +1,21 @@
+-- Folder Creation
+shell.run("mkdir", "dns")
+shell.run("mkdir", "packet")
+shell.run("mkdir", "server")
+
+-- Shared Libraries
+shell.run("wget", "https://raw.githubusercontent.com/icanthink42/mc_internet/main/shared/packet/main.lua",
+  "packet/main.lua")
+shell.run("wget", "https://raw.githubusercontent.com/icanthink42/mc_internet/main/shared/utils/config.lua",
+  "utils/config.lua")
+shell.run("wget", "https://raw.githubusercontent.com/icanthink42/mc_internet/main/shared/utils/main.lua",
+  "utils/main.lua")
+shell.run("wget", "https://raw.githubusercontent.com/icanthink42/mc_internet/main/shared/utils/sandbox.lua",
+  "utils/sandbox.lua")
+
+-- Server Install
+shell.run("wget", "https://raw.githubusercontent.com/icanthink42/mc_internet/main/server/server/main.lua",
+  "server/main.lua")
+shell.run("wget", "https://raw.githubusercontent.com/icanthink42/mc_internet/main/server/server/config.lua",
+  "server/config.lua")
+print("Server Installed! See for example websites: https://github.com/icanthink42/mc_internet/tree/main/server/server/examples")
